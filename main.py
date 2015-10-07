af9cb771e4f8e7f184b269a3bcb9a362aaa366e3
amingsc <amingsc@youremail.com>
2015-10-07 23:53:48 +0800

    x

--- main.py
+++ main.py
@@ -16,18 +16,26 @@ def index():
 @app.route('/userstory/list/')
 def list_userstory():
     data = {'project': model.get_project(),
-            'userstory': model.get_userstory()}
+            'userstory': model.get_userstory(with_frame=True),
+            'backlog': model.get_backlog(with_frame=True)}
     return render_template('userstory.html', data=data)
 
 @app.route('/userstory/add/', methods=['POST'])
-def ajax_add_new_us():
+def ajax_edit_us():
     try:
         data = {'name': request.form['name'],
                 'status': request.form['status'],
                 'priority': request.form['priority'],
                 'project_id': request.form['project_id']}
-        rowid = model.insert_userstory(data)
-        return json.dumps({'code': 0, 'rowid': rowid})
+        usid = request.form['userstory_id']
+        if usid:    # when update
+            rows = model.update_userstory(usid, data)
+            if not rows:
+                raise Exception(u'用户故事（id=%s）不存在！' % usid)
+            return json.dumps({'code': 0})
+        else:   # when insert
+            rowid = model.insert_userstory(data)
+            return json.dumps({'code': 0, 'rowid': rowid})
 
     except Exception as e:
         traceback.print_exc()
@@ -46,6 +54,18 @@ def ajax_delete_us():
         traceback.print_exc()
         return json.dumps({'code': 1, 'message': e.message})
 
+@app.route('/backlog/delete/', methods=['POST'])
+def ajax_delete_backlog():
+    try:
+        blid = request.form['blid']
+        rows = model.delete_backlog(blid)
+        if not rows:
+            raise Exception(u'Backlog（id=%s）不存在！' % blid)
+        return json.dumps({'code': 0})
+
+    except Exception as e:
+        traceback.print_exc()
+        return json.dumps({'code': 1, 'message': e.message})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- model.py
+++ model.py
@@ -8,7 +8,7 @@ methods for operate database.
 
 import sqlite3
 
-DBFILE = 'data.sqlite'  # under current directory
+DBFILE = 'data_dev.sqlite'  # under current directory
 
 
 def _dict_factory(cursor, row):
@@ -29,8 +29,11 @@ def get_project():
     return result
 
 
-def get_userstory():
-    'get the dataset of all userstories.'
+def get_userstory(with_frame=False):
+    '''get the dataset of all userstories.
+    @param with_frame: bool, if True then will return
+        one row presudo data for initlize empty web components.
+    '''
     conn = sqlite3.connect(DBFILE)
     conn.row_factory = _dict_factory
     cursor = conn.cursor()
@@ -41,10 +44,18 @@ def get_userstory():
             LEFT JOIN project p ON p.id=u.project_id"""
     cursor.execute(sql)
     result = cursor.fetchall()
+    if with_frame:
+        result.append({'id': -1, 'name': '', 'project_id': -1,
+                'status': u'正常', 'priority': u'低'})
     return result
 
 def insert_userstory(data):
     'insert new userstory'
+    assert data.status in ('正常', '取消', '暂停', '关闭'), \
+            '字段status输入值不合法: %s' % data.status
+    assert data.priority in ('低', '一般', '高', '紧急'), \
+            '字段priority输入值不合法: %s' % data.priority
+
     conn = sqlite3.connect(DBFILE)
     cursor = conn.cursor()
     sql = """INSERT INTO userstory(name,status,priority,project_id)
@@ -55,13 +66,61 @@ def insert_userstory(data):
     conn.close()
     return rowid
 
+def update_userstory(usid, data):
+    'update a already existed userstory'
+    conn = sqlite3.connect(DBFILE)
+    cursor = conn.cursor()
+    sql = """UPDATE userstory SET name=:name, status=:status,
+            priority=:priority, project_id=:project_id
+            WHERE id=:usid"""
+    data['usid'] = usid
+    cursor.execute(sql, data)
+    rows = conn.total_changes
+    conn.commit()
+    conn.close()
+    return rows
+
 def delete_userstory(usid):
     'delete a userstory'
     conn = sqlite3.connect(DBFILE)
     cursor = conn.cursor()
+    # 检查backlog是否为空
+    sql = "SELECT COUNT(1) FROM backlog WHERE userstory_id=?"
+    cursor.execute(sql, (usid,))
+    if cursor.fetchone()[0] > 0:
+        raise Exception('Backlog not empty!')
+    # 真正的删除操作
     sql = """DELETE FROM userstory WHERE id=?"""
     cursor.execute(sql, (usid,))
     rows = conn.total_changes
     conn.commit()
     conn.close()
     return rows
+
+def delete_backlog(blid):
+    'delete a backlog'
+    conn = sqlite3.connect(DBFILE)
+    cursor = conn.cursor()
+    sql = """DELETE FROM backlog WHERE id=?"""
+    cursor.execute(sql, (blid,))
+    rows = conn.total_changes
+    conn.commit()
+    conn.close()
+    return rows
+
+def get_backlog(with_frame=False):
+    '''get the dataset of all backlogs.
+    @param with_frame: bool, if True then will return
+        one row presudo data for initlize empty web components.
+    '''
+    conn = sqlite3.connect(DBFILE)
+    conn.row_factory = _dict_factory
+    cursor = conn.cursor()
+    sql = """SELECT b.id, b.name, b.progress, b.priority, b.userstory_id
+            FROM backlog b"""
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if with_frame:
+        result.append({'id': -1, 'name': '', 'userstory_id': -1,
+                'progress': u'未开始', 'priority': u'低'})
+    return result
