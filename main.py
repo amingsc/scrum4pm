9f2cb906e5e3cf6c0806605349edcb0438d6f0b5
amingsc <amingsc@youremail.com>
2015-10-18 16:35:02 +0800

    x

--- main.py
+++ main.py
@@ -10,37 +10,72 @@ app = Flask('scrum')
 
 @app.route('/')
 def index():
-    return  redirect(url_for('list_userstory'))
+    return  redirect(url_for('my_taskboard'))
 
-
-@app.route('/userstory/list/')
-def list_userstory():
+@app.route('/taskboard/')
+def my_taskboard():
+    '我的任务板'
     data = {'project': model.get_project(),
             'userstory': model.get_userstory(with_frame=True),
             'backlog': model.get_backlog(with_frame=True)}
-    return render_template('userstory.html', data=data)
+    return render_template('taskboard.html', data=data)
 
-@app.route('/userstory/add/', methods=['POST'])
-def ajax_edit_us():
+@app.route('/taskboard/update/', methods=['POST'])
+def ajax_task_update():
+    '更新任务'
     try:
         data = {'name': request.form['name'],
                 'status': request.form['status'],
                 'priority': request.form['priority'],
                 'project_id': request.form['project_id']}
-        usid = request.form['userstory_id']
-        if usid:    # when update
-            rows = model.update_userstory(usid, data)
-            if not rows:
-                raise Exception(u'用户故事（id=%s）不存在！' % usid)
-            return json.dumps({'code': 0})
-        else:   # when insert
-            rowid = model.insert_userstory(data)
-            return json.dumps({'code': 0, 'rowid': rowid})
+        subtask = json.loads(request.form['subtask'])
+        usid = request.form['id']
+        rows = model.update_userstory(usid, data, subtask)
+        if not rows:
+            raise Exception(u'用户故事（id=%s）不存在！' % usid)
+        return json.dumps({'code': 0})
 
     except Exception as e:
         traceback.print_exc()
         return json.dumps({'code': 1, 'message': e.message})
 
+
+@app.route('/ajax/taskboard/progress/save/', methods=['POST'])
+def ajax_task_updateprogress():
+    try:
+        data = {'progress': request.form['progress']}
+        subtask_id = request.form['subtask_id']
+        rows = model.update_subtask(subtask_id, data)
+        if not rows:
+            raise Exception(u'子任务（id=%s）不存在！' % subtask_id)
+        return json.dumps({'code': 0})
+
+    except Exception as e:
+        traceback.print_exc()
+        return json.dumps({'code': 1, 'message': e.message})
+
+@app.route('/review/')
+def daily_review():
+    '每日Review'
+    data = {'review_list': model.get_review_list(),
+            'today': model.get_today()}
+    return render_template('review.html', data=data)
+
+@app.route('/ajax/review/update/', methods=['POST'])
+def ajax_review_update():
+    try:
+        data = json.loads(request.form['reviewdata'])
+        model.update_review(data)
+        return json.dumps({'code': 0})
+
+    except Exception as e:
+        traceback.print_exc()
+        return json.dumps({'code': 1, 'message': e.message})
+
+
+##################
+# 下面代码作废
+##################
 @app.route('/userstory/delete/', methods=['POST'])
 def ajax_delete_us():
     try:
--- model.py
+++ model.py
@@ -6,6 +6,7 @@ methods for operate database.
 @version: 1.0
 """
 
+import datetime
 import sqlite3
 
 DBFILE = 'data_dev.sqlite'  # under current directory
@@ -66,19 +67,48 @@ def insert_userstory(data):
     conn.close()
     return rowid
 
-def update_userstory(usid, data):
+def update_userstory(usid, data, subtask):
     'update a already existed userstory'
     conn = sqlite3.connect(DBFILE)
-    cursor = conn.cursor()
-    sql = """UPDATE userstory SET name=:name, status=:status,
-            priority=:priority, project_id=:project_id
-            WHERE id=:usid"""
-    data['usid'] = usid
-    cursor.execute(sql, data)
-    rows = conn.total_changes
-    conn.commit()
-    conn.close()
-    return rows
+    try:
+        cursor = conn.cursor()
+
+        # 更新userstory(父任务)
+        if usid:    # update old
+            sql = """UPDATE userstory SET name=:name, status=:status,
+                    priority=:priority, project_id=:project_id
+                    WHERE id=:usid"""
+            data['usid'] = usid
+            cursor.execute(sql, data)
+            rows = conn.total_changes
+            assert rows==1, '用户故事（ID=%s）不存在' % usid
+        else:   # insert new
+            sql = """INSERT INTO userstory(name,status,priority,project_id)
+                    VALUES(:name, :status,:priority, :project_id)"""
+            cursor.execute(sql, data)
+            usid = cursor.lastrowid
+            rows = 1
+
+        # 更新子任务
+        for t in subtask:
+            if t['deleted']:
+                if not t['id']: continue
+                sql = "DELETE FROM backlog WHERE id=?"
+                cursor.execute(sql, (t['id']))
+            elif t['id']:
+                sql = "UPDATE backlog SET name=:name,priority=:priority WHERE id=:id"
+                cursor.execute(sql, t)
+            else:
+                sql = "INSERT INTO backlog(name,priority,userstory_id) VALUES(?,?,?)"
+                cursor.execute(sql, (t['name'], t['priority'], usid))
+
+        conn.commit()
+        conn.close()
+        return rows
+    except:
+        conn.rollback()
+        conn.close()
+        raise
 
 def delete_userstory(usid):
     'delete a userstory'
@@ -124,3 +154,66 @@ def get_backlog(with_frame=False):
         result.append({'id': -1, 'name': '', 'userstory_id': -1,
                 'progress': u'未开始', 'priority': u'低'})
     return result
+
+def update_subtask(subtask_id, data):
+    '更新子任务进度'
+    conn = sqlite3.connect(DBFILE)
+    cursor = conn.cursor()
+    sql = """UPDATE backlog SET progress=? WHERE id=?"""
+    cursor.execute(sql, (data['progress'], subtask_id))
+    rows = conn.total_changes
+    conn.commit()
+    conn.close()
+    return rows
+
+def get_review_list(days_limit=7, plan_limit=1):
+    '查询最近的review记录'
+    conn = sqlite3.connect(DBFILE)
+    conn.row_factory = _dict_factory
+    cursor = conn.cursor()
+
+    # 拼接连续日期的SQL
+    sql = "SELECT d.`date`, r.id, r.plan, r.done, r.quality, r.notes" \
+            " FROM (SELECT ? AS `date`"
+    for i in range(1, days_limit+plan_limit):
+        sql += " UNION ALL SELECT ? "
+    sql += ") d LEFT JOIN " \
+        " review r ON r.`date`=d.`date` " \
+        " ORDER BY d.`date` LIMIT %s" % (days_limit+plan_limit)
+    # 构造连续日期值
+    date_list = [datetime.date.today() - datetime.timedelta(i) \
+                    for i in range(-plan_limit,days_limit)]
+    date_list = [d.strftime('%Y-%m-%d') for d in date_list]
+
+    cursor.execute(sql, date_list)
+    result = cursor.fetchall()
+    return result
+
+def update_review(data):
+    '更新review属性'
+    try:
+        conn = sqlite3.connect(DBFILE)
+        cursor = conn.cursor()
+
+        # 查询已有review的日期
+        sql = "SELECT `date` FROM review LIMIT 30"
+        cursor.execute(sql)
+        date_list = set([r[0] for r in cursor.fetchall()])
+
+        sql1 = "UPDATE review SET plan=:plan, done=:done," \
+                "quality=:quality, notes=:notes" \
+                " WHERE `date`=:date"
+        sql2 = "INSERT INTO review(`date`,plan,done,quality,notes)" \
+                "VALUES(:date,:plan,:done,:quality,:notes)"
+        for r in data:
+            sql = (sql1, sql2)[r['date'] not in date_list]
+            cursor.execute(sql, r)
+        conn.commit()
+        conn.close()
+    except:
+        cursor.rollback()
+        conn.close()
+
+
+def get_today():
+    return datetime.date.today().strftime('%Y-%m-%d')
